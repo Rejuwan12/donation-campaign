c6b0fe75e157fd4863277245f43516599b647b75
Rejuwane <rezwanrian1@gmail.com>
2023-09-24 19:14:46 +0600

    setup with react router,tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-700 text-xl'>Donation Assignment</h1>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
